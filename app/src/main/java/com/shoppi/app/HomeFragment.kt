7e8c42d02c67eb7932c8af4951c54bf50a027464
Daeun <eldms0406@naver.com>
2023-02-25 19:54:58 +0900

    [feat] viewPager 어뎁터 연결, 파싱한 json 데이터 할당 및 viewPager와 tablayout(indicator) 연결

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -19,11 +25,38 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            val transaction = parentFragmentManager.beginTransaction()
-            transaction.add(R.id.container_main, ProductDetailFragment())
-            transaction.commit()
+        val toolbarTitle = view.findViewById<TextView>(R.id.tv_home_toolbar_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.iv_home_toolbar_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.vp_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.vp_home_banner_indicator)
+        val assetLoader = AssetLoader()
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+
+
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+            toolbarTitle.text = homeData.title.text
+            Glide.with(requireContext())
+                .load(homeData.title.iconUrl)
+                .into(toolbarIcon)
+
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+
+            TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+            }.attach()
         }
     }
 }
