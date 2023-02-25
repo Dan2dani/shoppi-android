00f4611dfadda82cee680671debe3bbb60d84453
Daeun <eldms0406@naver.com>
2023-02-25 19:52:59 +0900

    [feat] data object 생성

---
+++ app/src/main/java/com/shoppi/app/Banner.kt
@@ -0,0 +1,24 @@
+package com.shoppi.app
+
+import com.google.gson.annotations.SerializedName
+
+data class Banner(
+    @SerializedName("background_image_url") val backgroundImageUrl: String,
+    val badge: Badge,
+    val label: String,
+    @SerializedName("product_detail") val productDetail: ProductDetail
+)
+
+data class Badge(
+    val label: String,
+    @SerializedName("background_color") val backgroundColor: String
+)
+
+data class ProductDetail(
+    @SerializedName("brand_name") val brandName: String,
+    val label: String,
+    @SerializedName("discount_rate") val discountRate: Int,
+    val price: Int,
+    @SerializedName("thumbnail_image_url") val thumbnailImageUrl: String,
+    @SerializedName("product_id") val productId: String
+)
