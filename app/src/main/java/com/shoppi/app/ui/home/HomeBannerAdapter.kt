8c231a11ada1d6baefcc2c6d76fa7cf89e814634
Daeun <eldms0406@naver.com>
2023-03-14 22:52:20 +0900

    [feat] homeFragment 데이터 바인딩으로 구현

--- app/src/main/java/com/shoppi/app/ui/home/HomeBannerAdapter.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeBannerAdapter.kt
@@ -1,26 +1,19 @@
 package com.shoppi.app.ui.home
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.shoppi.app.data.model.Banner
 import com.shoppi.app.databinding.ItemHomeBannerBinding
-import java.text.DecimalFormat
-import kotlin.math.roundToInt
 
 class HomeBannerAdapter() :
     ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(BannerDiffCallback()) {
+    private lateinit var binding: ItemHomeBannerBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeBannerViewHolder {
-        val binding =
-            ItemHomeBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = ItemHomeBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HomeBannerViewHolder(binding)
     }
 
@@ -29,35 +22,11 @@ class HomeBannerAdapter() :
     }
 
     class HomeBannerViewHolder(
-        val binding: ItemHomeBannerBinding
+        private val binding: ItemHomeBannerBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(banner: Banner) {
-            loadImage(banner.backgroundImageUrl, binding.ivBannerImage)
-            binding.tvBannerBadge.text = banner.badge.label
-            binding.tvBannerBadge.background = ColorDrawable(Color.parseColor(banner.badge.backgroundColor))
-            binding.tvBannerTitle.text = banner.label
-            loadImage(banner.productDetail.thumbnailImageUrl, binding.ivBannerDetailThumbnail)
-            binding.tvBannerDetailBrandLabel.text = banner.productDetail.brandName
-            binding.tvBannerDetailProductLabel.text = banner.productDetail.label
-            binding.tvBannerDetailProductDiscountRate.text = "${banner.productDetail.discountRate}%"
-            calculateDiscountAmount(binding.tvBannerDetailProductDiscountPrice, banner.productDetail.discountRate, banner.productDetail.price)
-            applyPriceFormat(binding.tvBannerDetailProductPrice, banner.productDetail.price)
-        }
-
-        private fun calculateDiscountAmount(view: TextView, discountRate: Int, price: Int){
-            val discountPrice = ((100-discountRate) / 100.0 * price).roundToInt()
-            applyPriceFormat(view, discountPrice)
-        }
-
-        private fun applyPriceFormat(view: TextView, price: Int){
-            val decimalFormat = DecimalFormat("#,###")
-            view.text = decimalFormat.format(price) + "원"
-        }
-
-        private fun loadImage(urlString: String, imageView: ImageView){
-            Glide.with(itemView)
-                .load(urlString)
-                .into(imageView)
+            binding.banner = banner
+            binding.executePendingBindings()
         }
     }
 }
